0e25df02cbfa5f1266c0ed6770257c25dbb48a26
pLe98 <lelkadaparami@gmail.com>
2021-10-03 07:59:13 +0530

    mmse quiz

--- app/dashchat/layout.py
+++ app/dashchat/layout.py
@@ -2,6 +2,7 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
 
+
 def textbox(text, box="other"):
     style = {
         "max-width": "55%",
@@ -30,21 +31,79 @@ def textbox(text, box="other"):
 
     return dbc.Card(text, style=style, body=True, color=color, inverse=inverse)
 
-conversation = html.Div( style={
-        "width": "80%",
-        "max-width": "800px",
-        "height": "78vh",
-        "margin": "auto",
-        "overflow-y": "auto",
-        "padding": "10px 5px",
-    },
+
+conversation = html.Div(style={
+    "width": "80%",
+    "max-width": "800px",
+    "height": "78vh",
+    "margin": "auto",
+    "overflow-y": "auto",
+    "padding": "10px 5px",
+},
     id="display-conversation",)
+
+quizmodal = html.Div(
+    [
+        dbc.Button("Open", id="open-centered"),
+        dbc.Modal(
+            [
+                dbc.ModalHeader("Header123"),
+                dbc.ModalBody(
+
+                    html.Div([
+                        html.Div(id='body-div'),
+
+                        html.Br(),
+                        html.Div(
+                            [html.Img(
+                                id="img_watch",
+                                src='',
+                                height = '200px',
+                                width = '300px',
+                                style={'height': '10 rem', 'width': '10 rem', 'display': 'none'})
+                             ], style={'max-height': '20rem',
+                                       'max-width': '20rem',
+                                       'height':'auto',
+                                       'width':'auto', 
+                                       'display': 'block', 
+                                       'justifyContent': 'center',
+                                       'object-fit': 'fill'}
+                        ),
+
+                        dbc.Input(id="quizinput",
+                                  placeholder="Type something...", type="text"),
+
+                        html.H4(id='score_output'),
+                        html.Br(),
+                        html.Button('Next', id='nextbutton', n_clicks=0,),
+
+                    ]),
+                ),
+                dbc.ModalFooter(
+
+                    dbc.Button(
+                        "Close",
+                        id="close-centered",
+                        className="ml-auto",
+                        n_clicks=0,
+                    )
+
+                ),
+            ],
+            id="modal-centered",
+            centered=True,
+
+        ),
+    ]
+)
 
 controls = dbc.InputGroup(
     style={"width": "80%", "max-width": "800px", "margin": "auto"},
     children=[
-        dbc.Input(id="user-input", placeholder="Write to the chatbot...", type="text"),
-        dbc.InputGroupAddon(dbc.Button("Submit", id="submit"), addon_type="append",),
+        dbc.Input(id="user-input",
+                  placeholder="Write to the chatbot...", type="text"),
+        dbc.InputGroupAddon(dbc.Button(
+            "Submit", id="submit"), addon_type="append",),
     ],
 )
 
@@ -52,17 +111,20 @@ controls = dbc.InputGroup(
 layout = dbc.Container(
     fluid=True,
     children=[
-        html.H1("Chatbot", style={"margin": "auto","text-align" : "center"}),
+        html.H1("Chatbot", style={"margin": "auto", "text-align": "center"}),
         html.Hr(),
         dcc.Store(id="store-conversation", data=[]),
         dcc.Store(id="store-qcount", data=0),
         dcc.Store(id="store-ans", data=[]),
+        dcc.Store(id="quizcount", data=0),
+        dcc.Store(id="score", data=0),
+        quizmodal,
         conversation,
         controls,
         dcc.Interval(
             id='onload_delay',
             max_intervals=1,
-            interval=1*1000, # in milliseconds
+            interval=1*1000,  # in milliseconds
             n_intervals=0)
     ],
 )
